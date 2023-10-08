69d28c40c45aec23157295a48dcc64a4315c6dbe
Shahriar Rahman <shahriar.rahman10@northsouth.edu>
2023-10-08 21:37:12 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='python_utils',
+    version='1.0.1',
+    author='Shahriar Rahman',
+    author_email='shahriarrahman1101@gmail.com',
+    description='A Python package that includes several common Python util functions.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
